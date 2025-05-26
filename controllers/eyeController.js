c01ce5defe1159628d32f59243231b2f6f173076
rahulapps29 <rahulapps29@gmail.com>
2025-05-27 00:48:22 +0530

    second commit

--- controllers/eyeController.js
+++ controllers/eyeController.js
@@ -1,6 +1,7 @@
 const EyeEntry = require("../models/EyeEntry");
 const User = require("../models/User");
 
+// Create Eye Entry
 exports.createEntry = async (req, res) => {
   try {
     const entry = new EyeEntry({ ...req.body, user: req.user._id });
@@ -11,6 +12,7 @@ exports.createEntry = async (req, res) => {
   }
 };
 
+// Share Entry With Users by Email
 exports.shareEntry = async (req, res) => {
   try {
     const { emails } = req.body;
@@ -20,34 +22,42 @@ exports.shareEntry = async (req, res) => {
     const users = await User.find({ email: { $in: emails } });
     const userIds = users.map((u) => u._id);
 
+    // Avoid duplicates
     entry.sharedWith.push(
       ...userIds.filter((id) => !entry.sharedWith.includes(id))
     );
+
     await entry.save();
 
-    res
-      .status(200)
-      .json({
-        message: "Entry shared successfully",
-        sharedWith: entry.sharedWith,
-      });
+    res.status(200).json({
+      message: "Entry shared successfully",
+      sharedWith: entry.sharedWith,
+    });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
+// Get Entries Created by Logged-in User
 exports.getMyEntries = async (req, res) => {
   try {
-    const entries = await EyeEntry.find({ user: req.user._id });
+    const entries = await EyeEntry.find({ user: req.user._id }).populate(
+      "sharedWith",
+      "email firstName"
+    );
     res.status(200).json(entries);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
+// Get Entries Shared With Logged-in User
 exports.getSharedEntries = async (req, res) => {
   try {
-    const entries = await EyeEntry.find({ sharedWith: req.user._id });
+    const entries = await EyeEntry.find({ sharedWith: req.user._id }).populate(
+      "user",
+      "firstName lastName email"
+    );
     res.status(200).json(entries);
   } catch (err) {
     res.status(400).json({ error: err.message });
